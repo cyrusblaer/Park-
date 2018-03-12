88fab247ca693734f57016fed6da4b0fa2f821b3
zhangxi <zhangxi@gzkit.com.cn>
2018-03-12 19:43:00 +0800

    接入第三方SDK

---
+++ Park!/Maps/Services/AMap-Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  AMap-Bridge-Header.h
+//  Park!
+//
+//  Created by Blaer on 12/03/2018.
+//  Copyright © 2018 Blaer. All rights reserved.
+//
+
+#ifndef AMap_Bridge_Header_h
+#define AMap_Bridge_Header_h
+
+#import <MAMapKit/MAMapKit.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
+
+#endif /* AMap_Bridge_Header_h */
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Park!' do
+
+pod 'RAMAnimatedTabBarController'
+pod 'SnapKit', '~> 4.0.0'
+pod 'Wilddog/Sync'
+pod 'AMap2DMap' #2D地图SDK
+pod 'AMapSearch' #搜索功能
+    
+end
